760c7c045d296506266bb8ba3b73be6babdb03e4
Garrett <gflynn@usc.edu>
2021-01-02 17:07:43 -0600

    Added p5 template

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,8 @@
 const path = require("path");
 
 const home = (req, res) => {
-  return res.redirect('https://brainsatplay.com')
-  // return res.sendFile(path.join(`${__dirname}/../public/index.html`));
+  // return res.redirect('https://brainsatplay.com')
+  return res.sendFile(path.join(`${__dirname}/../public/index.html`));
 };
 
 module.exports = {
