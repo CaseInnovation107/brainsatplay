b7aae0aa7166ca82fb3d6f46cc00b571d1f31de2
Garrett <gflynn@usc.edu>
2021-02-17 20:57:20 -0600

    Updated to remove React dependency

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/app.mjs',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, 'libraries', 'js'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "script-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
